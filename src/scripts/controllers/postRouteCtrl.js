179e7dffb5715c70a42a3d46796f0dbc26967a25
fredericJos <fredericaerts@hotmail.com>
2015-05-24 17:17:25 +0200

    series pagination range and refactoring routePostCtrl

--- src/scripts/controllers/postRouteCtrl.js
+++ src/scripts/controllers/postRouteCtrl.js
@@ -1,84 +1,21 @@
-blogApp.controller("postRouteCtrl", ["$scope", "$routeParams", "blogPostsFactory", "POSTS_ROOT", function DefaultCtrl($scope, $routeParams, blogPostsFactory, POSTS_ROOT) {
+blogApp.controller("postRouteCtrl", ["$scope", "$routeParams", "blogPostsFactory", function DefaultCtrl($scope, $routeParams, blogPostsFactory) {
 	'use strict';
 
-	// pagination
-	$scope.itemsPerPage = 5;
-	$scope.currentPage = 0;
-	$scope.items = [];
-
-	for (var i=0; i<50; i++) {
-		$scope.items.push({ id: i, name: "name "+ i, description: "description " + i });
-	}
-
-	$scope.range = function() {
-		var rangeSize = 5;
-		var ret = [];
-		var start;
-
-		start = $scope.currentPage;
-		if ( start > $scope.pageCount()-rangeSize ) {
-			start = $scope.pageCount()-rangeSize+1;
-		}
-
-		for (var i=start; i<start+rangeSize; i++) {
-			ret.push(i);
-		}
-		return ret;
-	};
-
-	
-
-	$scope.pageCount = function() {
-		return Math.ceil($scope.items.length/$scope.itemsPerPage)-1;
-	};
-
-	
-
-	$scope.setPage = function(n) {
-		$scope.currentPage = n;
-	};
-	//end pagination
-
-	var setupPromisedScope, getArticleData, getSeriesArticles, setupSeriesPagination;
+	var getCurrentArticle, getSeriesArticles, setupSeriesPagination;
 
 	var promise = blogPostsFactory().then(function(data) {
-		setupPromisedScope(data);
-	}, function(error) {
-		console.log("promise for blogposts json http request failed to resolve in postRouteCtrl \nError: " + error);
-	});
+		$scope.currentArticle = getCurrentArticle(data.blogPosts, $routeParams.articleRouteParam);
 
-	setupPromisedScope = function(data) {
-		$scope.currentArticle = getArticleData(data.blogPosts, $routeParams.articleRouteParam);
-		
 		if($scope.currentArticle.partOfSeries) {
 			var seriesArticles = getSeriesArticles(data.blogPosts, $scope.currentArticle.seriesId);
 			setupSeriesPagination(seriesArticles);
 		}
-	};
 
-	setupSeriesPagination = function(seriesArticles) {
-		$scope.prevPage = function() {
-			if ($scope.currentArticle.seriesIndex > 0) {
-				$scope.currentArticle = seriesArticles[$scope.currentArticle.seriesIndex - 1];
-			}
-		};
+	}, function(error) {
+		console.log("promise for blogposts json http request failed to resolve in postRouteCtrl \nError: " + error);
+	});
 
-		$scope.nextPage = function() {
-			if ($scope.currentArticle.seriesIndex < seriesArticles.length - 1) {
-				$scope.currentArticle = seriesArticles[$scope.currentArticle.seriesIndex + 1];
-			}
-		};
-
-		$scope.prevPageDisabled = function() {
-			return $scope.currentArticle.seriesIndex === 0 ? "disabled" : "";
-		};
-
-		$scope.nextPageDisabled = function() {
-			return $scope.currentArticle.seriesIndex === seriesArticles.length - 1 ? "disabled" : "";
-		};
-	};
-
-	getArticleData = function(blogPosts, routeParam) {
+	getCurrentArticle = function(blogPosts, routeParam) {
 		var article = {};
 
 		for (var i = 0; i < blogPosts.length; i++) {
@@ -104,5 +41,42 @@ blogApp.controller("postRouteCtrl", ["$scope", "$routeParams", "blogPostsFactory
 			return (a.seriesIndex - b.seriesIndex); 
 		});
 		return seriesArticles;
+	};
+
+	setupSeriesPagination = function(seriesArticles) {
+		var numberOfArticles = seriesArticles.length;
+
+		$scope.prevPage = function() {
+			if ($scope.currentArticle.seriesIndex > 0) {
+				$scope.currentArticle = seriesArticles[$scope.currentArticle.seriesIndex - 1];
+			}
+		};
+
+		$scope.nextPage = function() {
+			if ($scope.currentArticle.seriesIndex < numberOfArticles - 1) {
+				$scope.currentArticle = seriesArticles[$scope.currentArticle.seriesIndex + 1];
+			}
+		};
+
+		$scope.prevPageDisabled = function() {
+			return $scope.currentArticle.seriesIndex === 0 ? "disabled" : "";
+		};
+
+		$scope.nextPageDisabled = function() {
+			return $scope.currentArticle.seriesIndex === numberOfArticles - 1 ? "disabled" : "";
+		};
+
+		$scope.range = function() {
+			var rangeArray = [];
+
+			for (var i = 0; i < numberOfArticles; i++) {
+				rangeArray.push(i);
+			}
+			return rangeArray;
+		};
+
+		$scope.setPage = function(n) {
+			$scope.currentArticle = seriesArticles[n];
+		};
 	};
 }]);
